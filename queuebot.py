4024f1ff490ba5d28e6227b9d0eb0f82238ab081
Ann Pidchasiuk <47794397+pidchasiuk@users.noreply.github.com>
2020-05-04 22:28:57 +0300

    added new tags

--- queuebot.py
+++ queuebot.py
@@ -4,37 +4,23 @@ import dbqueue as dbq
 bot = telebot.TeleBot('1058265955:AAEhi2xia18dZCRuDPnZYB7NE5ZzAFxehtQ')
 
 
-def add_id(id: int):
-    id += 1
-    return id
-
-
-def add_number(number: int):
-    number += 1
-    return number
-
-
-def format_queue(number, name):
-    return '{0} - {1}'.format(number, name)
-
-
 def send_queue():
-    # subject = 'Предмет:' + '\n'
-    # date = 'Дата:' + '\n'
-    queue = 'Актуальна черга:' + '\n'
-    res = dbq.get_name_number()
+    res = dbq.get_id_name()
     res_message = ''
-    for i in res:
-        res_message += (i + '\n')
-    print(res_message)
+    for step in res:
+            temp = '{0} - {1}'.format(res[step][0], res[step][1])
+            res_message += temp + '\n'
+    return res_message
 
 
 if __name__ == '__main__':
-    dbq.get_name_number()
+    # dbq.get_id_name()
+    send_queue()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    dbq.del_all_users()
     bot.send_message(message.chat.id, 'Давай створимо чергу. \n'
                                       'Щоб зайняти місце натисни /reg \n'
                                       'Коли чергу буде сформовано натисни /end')
@@ -43,10 +29,8 @@ def start_message(message):
 @bot.message_handler(commands=['reg'])
 def reg_message(message):
     """Додавання користувача в базу даних і в чергу"""
-    i = 0
-    dbq.add_user(add_id(i), add_number(i), message.from_user.username)
+    dbq.add_user(message.from_user.username)
     bot.send_message(message.chat.id, 'Ти в черзі')
-    i += 1
 
 
 @bot.message_handler(commands=['end'])
@@ -56,11 +40,26 @@ def end_message(message):
                                       'Щоб переглянути чергу натисни /check\n')
 
 
+@bot.message_handler(commands=['check'])
+def check_message(message):
+    """Перевірка черги. Вивід списку учасників"""
+    bot.send_message(message.chat.id, send_queue())
+
+
+@bot.message_handler(commands=['del'])
+def del_message(message):
+    """Видалення користувача з черги і бази даних"""
+    dbq.del_user(message.from_user.username)
+    bot.send_message(message.chat.id, 'Тебе видалено з черги')
+
+
 @bot.message_handler(commands=['inform'])
 def inform_message(message):
     """Вивід інформації про можливості бота."""
     bot.send_message(message.chat.id, 'Щоб розпочати створення черги натисни /start\n'
                                       'Щоб зайняти місце натисни /reg\n'
                                       'Щоб зупинити процес створення черги натисни /end\n'
+                                      'Щоб видалити себе із черги натисни /del\n'
                                       'Щоб переглянути список натисни /check')
-bot.polling()
+
+# bot.polling()
